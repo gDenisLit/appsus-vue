cd80c0159f29bb1bc2b7cddbd74856bd4b7db982
Denis Lit <denislit@gmail.com>
2022-06-29 16:58:55 +0300

    add some styling to side nav

--- js/apps/emails/cmps/email-side.cmp.js
+++ js/apps/emails/cmps/email-side.cmp.js
@@ -1,12 +1,12 @@
 export default {
     template: `
         <section class="side-nav flex column">
-            <button>+Compose</button>
-            <button>Inbox</button>
-            <button>Starred</button>
-            <button>Sent</button>
-            <button>Drafts</button>
-            <button>Trash</button>
+            <button class="email-compose">+Compose</button>
+            <button class="nav-btn">Inbox</button>
+            <button class="nav-btn">Starred</button>
+            <button class="nav-btn">Sent</button>
+            <button class="nav-btn">Drafts</button>
+            <button class="nav-btn">Trash</button>
         </section>
     `,
  
