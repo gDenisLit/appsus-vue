68c1656ad2233e55fb5cd86d81fda857a2342456
Guy Kadosh <guyk50@gmail.com>
2022-06-29 16:15:25 +0300

    Added color palette functionality

--- js/apps/keeps/cmps/note-preview.cmp.js
+++ js/apps/keeps/cmps/note-preview.cmp.js
@@ -2,14 +2,16 @@ import noteImg from './costumes/note-img.cmp.js'
 import noteTxt from './costumes/note-txt.cmp.js'
 import noteTodos from './costumes/note-todos.cmp.js'
 import noteVideo from './costumes/note-video.cmp.js'
+import noteTools from './note-tools.cmp.js'
 
 export default {
   props: ['note'],
   template: `
-      <section class="note-preview">
+      <section class="note-preview" :style="bgc">
         <component class="note" :is="note.type"  
           :info="note.info">
         </component>
+        <note-tools :note="note" />
       </section>
     `,
   components: {
@@ -17,12 +19,17 @@ export default {
     noteTodos,
     noteTxt,
     noteVideo,
+    noteTools,
   },
   data() {
     return {}
   },
   created() {},
   methods: {},
-  computed: {},
+  computed: {
+    bgc() {
+      return { backgroundColor: this.note.style.backgroundColor }
+    },
+  },
   unmounted() {},
 }
--- js/apps/keeps/cmps/note-tools.cmp.js
+++ js/apps/keeps/cmps/note-tools.cmp.js
@@ -1,12 +1,37 @@
-export default {
-  template: `
+import noteColorPalette from './note-color-palette.cmp.js'
+import { changeBgcEmit } from '../../../services/eventBus.service.js'
 
+export default {
+  props: ['note'],
+  template: `
+      <section class="note-tools flex">
+        <a><i class="fa-solid fa-thumbtack"></i></a>
+        <a class="color-palette-btn">
+          <span>
+            <i class="fa-solid fa-palette"></i>
+          </span>
+            <note-color-palette @picked="changeBgc" />
+        </a>
+        <a><i class="fa-solid fa-pen-to-square"></i></a>
+        <a><i class="fa-solid fa-clone"></i></a>
+        <a><i class="fa-solid fa-envelope-open-text"></i></a>
+        <a><i class="fa-solid fa-trash-can"></i></a>
+      </section>
     `,
+  components: {
+    noteColorPalette,
+  },
   data() {
-    return {}
+    return {
+      showColors: false,
+    }
   },
   created() {},
-  methods: {},
+  methods: {
+    changeBgc(color) {
+      changeBgcEmit(color, this.note.id)
+    },
+  },
   computed: {},
   unmounted() {},
 }
