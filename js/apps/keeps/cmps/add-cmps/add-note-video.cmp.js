62286295325ef7d4485729618ec421b68710109d
Guy Kadosh <guyk50@gmail.com>
2022-06-29 20:00:48 +0300

    added add videp cmp

--- js/apps/keeps/cmps/add-cmps/add-note-video.cmp.js
+++ js/apps/keeps/cmps/add-cmps/add-note-video.cmp.js
@@ -4,7 +4,7 @@ export default {
   template: ` 
     <section class="note-add" @keyup.enter="addNote">
       <div class="add-note-txt flex">
-        <input v-model="note.info.url" type="url" placeholder="Enter video url...">
+        <input v-model="url" type="url" placeholder="Enter video url from YouTube...">
       </div>
     </section>
 `,
@@ -13,17 +13,32 @@ export default {
       note: {
         type: 'note-video',
         info: {
-          url: '',
+          videoId: '',
         },
       },
+      url: '',
     }
   },
   created() {},
   methods: {
     addNote() {
       // const newNote = JSON.parse(JSON.stringify(this.note))
+      const videoId = this.extractId(this.url)
+      if (!videoId) {
+        // Show user error msg user note found
+        return
+      }
+
+      this.note.info.videoId = videoId
       addNoteEmit(this.note)
       this.$emit('added')
+    },
+    extractId(url) {
+      const regExp =
+        /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
+      const match = url.match(regExp)
+
+      return match && match[7].length == 11 ? match[7] : null
     },
   },
   computed: {},
