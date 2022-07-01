ae35ca70eddad4f85a4f79fcf86040a6a3fad957
Denis Lit <denislit@gmail.com>
2022-07-01 20:30:58 +0300

    add advanced filter layout and styling

--- js/apps/emails/cmps/email-filter.cmp.js
+++ js/apps/emails/cmps/email-filter.cmp.js
@@ -13,12 +13,30 @@ export default {
             <input class="input-search" type="text" v-model="filterBy.txt" @input="filter" placeholder="Search">
             <i class="icon fa-solid fa-magnifying-glass"></i>
             <i class="fa-solid fa-sliders" @click="showDropDown"></i>
+            
             <div class="adv-filter" v-if="isDropDown">
-              <h1>Sort By</h1>
-              <div class="by-date">
-                <input class="sort-date" type="checkbox" v-model="sortBy.date" @change="sort">
-                <span>By Date</span>
-                <i class="fa-solid fa-arrow-up-z-a" @click="sortDir"></i>
+                <div class="sort-date" @click="sort">
+                  <span class="label">Date</span>
+                  <i class="fa-solid fa-arrow-up-z-a" @click="sortDir"></i>
+                </div>
+                <div class="flex">
+                  <span class="label">From</span>
+                  <input type="search">
+                </div>
+                <div class="flex">
+                  <span class="label">Subject</span>
+                  <input type="search">
+                </div>
+                <div class="flex"> 
+                  <span class="label">Has the words</span>
+                  <input type="search">
+                </div>
+                <div class="flex">
+                  <span class="label">Date within</span>
+                  <input class="date-from" type="date">
+                  <input type="date">
+                </div>
+                <!-- <input class="sort-date" type="checkbox" v-model="sortBy.date" @change="sort"> -->
               </div>
             </div>
           </div>
@@ -42,6 +60,7 @@ export default {
       this.$emit('search', this.filterBy.txt)
     },
     sort() {
+      this.sortBy.date = !this.sortBy.date
       this.$emit('sort', this.sortBy)
     },
     sortDir() {
