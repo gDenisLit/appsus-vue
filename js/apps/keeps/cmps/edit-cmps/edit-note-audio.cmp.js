7540a3e47b93cb00487df87489da5cd261cb7d53
Guy Kadosh <guyk50@gmail.com>
2022-07-02 15:19:19 +0300

    updated note-edit design

--- js/apps/keeps/cmps/edit-cmps/edit-note-audio.cmp.js
+++ js/apps/keeps/cmps/edit-cmps/edit-note-audio.cmp.js
@@ -3,8 +3,7 @@ import { updateNoteEmit } from '../../../../services/eventBus.service.js'
 export default {
   props: ['note'],
   template: `
-        <section class="note-add">
-          <div class="add-note-img">
+        <section class="note-edit-audio">
             <audio controls>
               <source :src="note.info.url" type="audio/mp3">
               Your browser does not support the audio element.
@@ -14,15 +13,37 @@ export default {
              placeholder="Pick a title first"
              @change="updateNote">
             
-             <div class="">
-               <input type="url"
-               v-model="note.info.url"
-               placeholder="Enter audio url"
-               @keyup.enter="updateNote">
-               <span>Or</span>
-               <input type="file" @change="audioInput">
+             <div class="audio-inputs flex">
+
+           <div>
+              <input type="url"
+                v-model="note.info.url"
+                placeholder="Enter audio url"
+                @keyup.enter="addNote()">
               </div>
-          </div>
+
+                <div>
+                  <span>Or </span>
+                  <label for="file-upload" class="file-upload">
+                    <i class="fa-solid fa-upload"></i>
+                    <span>Choose file</span>
+                  </label>
+                  <input type="file" id="file-upload" @change="audioInput">
+                </div>
+
+                <!-- <div class="recorder-container flex">
+                  <span>Or record yourself</span>
+                  <div class="recorder flex">
+                    <button :style="recordStyle" :disabled="recordBtn" @click="startRecording"><i class="fa-solid fa-record-vinyl"></i></button>
+                    <button :disabled="pauseBtn" @click="pauseRecording">
+                      <i v-if="!isPaused" class="fa-solid fa-pause"></i>
+                      <i v-else class="fa-solid fa-play"></i>
+                    </button>
+                    <button @click="stopRecording" :disabled="stopBtn"><i class="fa-solid fa-stop"></i></button>
+                 </div> -->
+
+                </div>
+            </div>    
         </section>
     `,
   data() {
